60d09ef247cece5ee5294946076e983ee37621ce
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-04-14 22:31:03 -0700

    examples

--- bin/iceberg-examples.sql
+++ bin/iceberg-examples.sql
@@ -3,17 +3,6 @@ SELECT *
 FROM titanic_merged_packages
 LIMIT 10;
 
--- Query with time travel (as of timestamp)
-SELECT * 
-FROM "titanic_merged_packages" TIMESTAMP AS OF TIMESTAMP '2025-04-14 12:00:00'
-WHERE source_bucket = 'test-bucket'
-LIMIT 10;
-
--- Query with time travel (as of snapshot ID)
-SELECT * 
-FROM "titanic_merged_packages" VERSION AS OF 1234567
-LIMIT 10;
-
 -- Join packages and objects
 SELECT 
   p.pkg_name,
@@ -26,7 +15,7 @@ FROM titanic_merged_packages p
 JOIN titanic_merged_objects o 
   ON p.pkg_name = o.pkg_name 
   AND p.top_hash = o.top_hash
-WHERE p.source_bucket = 'test-bucket'
+WHERE p.source_bucket = 'quilt-bake'
 LIMIT 10;
 
 -- Show table history
@@ -56,3 +45,14 @@ CALL system.optimize('titanic_merged_objects');
 -- Expire old snapshots (cleanup)
 CALL system.expire_snapshots('titanic_merged_packages', TIMESTAMP '2025-04-07 00:00:00');
 CALL system.expire_snapshots('titanic_merged_objects', TIMESTAMP '2025-04-07 00:00:00');
+
+-- Query with time travel (as of timestamp)
+SELECT * 
+FROM "titanic_merged_packages" TIMESTAMP AS OF TIMESTAMP '2025-04-14 12:00:00'
+WHERE source_bucket = 'quilt-bake'
+LIMIT 10;
+
+-- Query with time travel (as of snapshot ID)
+SELECT * 
+FROM "titanic_merged_packages" VERSION AS OF 1234567
+LIMIT 10;
